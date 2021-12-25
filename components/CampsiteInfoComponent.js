277e8a9f3509bbea31ca46abf1c4c4a570b5d2f0
Cody Lillywhite <codylillyw@gmail.com>
2021-12-24 23:06:05 -0500

    Redux Adding Favorites

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -3,12 +3,18 @@ import { Text, View, ScrollView, FlatList } from "react-native";
 import { Card, Icon } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { postFavorite } from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
   return {
     campsites: state.campsites,
     comments: state.comments,
+    favorites: state.favorites,
   };
+};
+
+const mapDispatchToProps = {
+  postFavorite: (campsiteId) => postFavorite(campsiteId),
 };
 
 function RenderComments({ comments }) {
@@ -56,17 +62,11 @@ function RenderCampsite({ campsite, favorite, markFavorite }) {
 }
 
 class CampsiteInfo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      favorite: false,
-    };
-  }
   static navigationOptions = {
     title: "Campsite Information",
   };
-  markFavorite() {
-    this.setState({ favorite: true });
+  markFavorite(campsiteId) {
+    this.props.postFavorite(campsiteId);
   }
   render() {
     const campsiteId = this.props.navigation.getParam("campsiteId");
@@ -80,12 +80,12 @@ class CampsiteInfo extends Component {
       <ScrollView>
         <RenderCampsite
           campsite={campsite}
-          favorite={this.state.favorite}
-          markFavorite={() => this.markFavorite()}
+          favorite={this.props.favorites.includes(campsiteId)}
+          markFavorite={() => this.markFavorite(campsiteId)}
         />
         <RenderComments comments={comments} />
       </ScrollView>
     );
   }
 }
-export default connect(mapStateToProps)(CampsiteInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { campsites } from './campsites';
-import { comments } from './comments';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { campsites } from "./campsites";
+import { comments } from "./comments";
 import { promotions } from "./promotions";
 import { partners } from "./partners";
+import { favorites } from "./favorites";
 
 export const ConfigureStore = () => {
   const store = createStore(
@@ -12,9 +13,10 @@ export const ConfigureStore = () => {
       campsites,
       comments,
       promotions,
-      partners
+      partners,
+      favorites,
     }),
     applyMiddleware(thunk, logger)
   );
   return store;
-}
+};
