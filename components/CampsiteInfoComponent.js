0d74d8b5ee9afc66c4f5d6693bb1bf7c98f99c0c
Cody Lillywhite <codylillyw@gmail.com>
2021-12-29 09:19:19 -0500

    Animatable

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, Icon, Input, Rating } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import { postFavorite, postComment } from "../redux/ActionCreators";
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = (state) => {
   return {
@@ -35,45 +36,51 @@ function RenderComments({ comments }) {
     );
   };
   return (
-    <Card title="Comments">
-      <FlatList
-        data={comments}
-        renderItem={renderCommentItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </Card>
+    <Animatable.View animation={'fadeInUp'} duration={2000} delay={1000}>
+
+      <Card title="Comments">
+        <FlatList
+          data={comments}
+          renderItem={renderCommentItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </Card>
+    </Animatable.View>
   );
 }
 
 function RenderCampsite({ campsite, favorite, markFavorite, onShowModal }) {
   if (campsite) {
     return (
-      <Card
-        featuredTitle={campsite.name}
-        image={{ uri: baseUrl + campsite.image }}>
-        <Text style={{ margin: 10 }}>{campsite.description}</Text>
-        <View style={styles.cardRow}>
-        <Icon
-          name={favorite ? "heart" : "heart-o"}
-          type="font-awesome"
-          color="#f50"
-          raised
-          onPress={() =>
-            favorite ? console.log("already set as a favorite") : markFavorite()
-          }
-        />
-        <Icon
-          name={"pencil"}
-          type="font-awesome"
-          color="#5637DD"
-          raised
-          reverse
-          onPress={() =>
-            onShowModal()
-          }
-        />
-        </View>
-      </Card>
+      <Animatable.View animation={'fadeInDown'} duration={2000} delay={1000}>
+
+        <Card
+          featuredTitle={campsite.name}
+          image={{ uri: baseUrl + campsite.image }}>
+          <Text style={{ margin: 10 }}>{campsite.description}</Text>
+          <View style={styles.cardRow}>
+          <Icon
+            name={favorite ? "heart" : "heart-o"}
+            type="font-awesome"
+            color="#f50"
+            raised
+            onPress={() =>
+              favorite ? console.log("already set as a favorite") : markFavorite()
+            }
+          />
+          <Icon
+            name={"pencil"}
+            type="font-awesome"
+            color="#5637DD"
+            raised
+            reverse
+            onPress={() =>
+              onShowModal()
+            }
+          />
+          </View>
+        </Card>
+      </Animatable.View> 
     );
   }
   return <View />;
--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -7,6 +7,7 @@ import { baseUrl } from '../shared/baseUrl';
 import { SwipeRow } from 'react-native-swipe-list-view'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { campsitesLoading, deleteFavorite } from '../redux/ActionCreators'
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -73,13 +74,15 @@ class Favorites extends Component {
             );
         }
         return (
-            <FlatList
-                data={this.props.campsites.campsites.filter(
-                    campsite => this.props.favorites.includes(campsite.id)
-                )}
-                renderItem={renderFavoriteItem}
-                keyExtractor={item => item.id.toString()}
-            />
+            <Animatable.View animation={"fadeInRightBig"} duration={2000}>
+                <FlatList
+                    data={this.props.campsites.campsites.filter(
+                        campsite => this.props.favorites.includes(campsite.id)
+                    )}
+                    renderItem={renderFavoriteItem}
+                    keyExtractor={item => item.id.toString()}
+                />
+            </Animatable.View>
         );
     }
 }
